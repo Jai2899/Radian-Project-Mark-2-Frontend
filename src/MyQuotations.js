5e9fc1440d3db07332eb7c51826c02e20cce22f7
ahdiehemadi <ahdieh_e@yahoo.com>
2021-11-16 14:50:16 -0500

    Modifying the Styling of MyQuotations page

--- src/MyQuotations.js
+++ src/MyQuotations.js
@@ -1,6 +1,8 @@
 import { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "./context/auth-context";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 const MyQuotations = () => {
   const context = useContext(AuthContext);
   const [isLoading, setIsLoading] = useState(true);
@@ -15,64 +17,7 @@ const MyQuotations = () => {
           address
           purchaseAmount
           loanAmount
-          QuoteDate
-          rebate
-          originalTitleQuote
-          rebateSavings
-          radianQuote
-          BuyerResponsibility
-          sellerResponsibility
-          SOTI
-          SLTI
-          SBR
-          STPremium
-          SAE906
-          SAE8106
-          STEndorsements
-          SCF
-          SSF
-          STSettlementCharges
-          STitleCharges
-          BCF
-          BSF
-          BTSettlementCharges
-          BAE906
-          BAE8106
-          BTEndorsements
-          BLTI
-          BBR
-          BOTI
-          BTPremium
-          BTitleCharges
-          TOTI
-          TLTI
-          TBR
-          TTPremium
-          TAE906
-          TAE8106
-          TTEndorsements
-          TCF
-          TSF
-          TTSettlementCharges
-          TTitleCharges
-          BDST
-          BMST
-          BMIT
-          BMRF
-          BDRF
-          BTTOGF
-          SMST
-          SDST
-          SMIT
-          SMRF
-          SDRF
-          STTOGF
-          TDST
-          TMST
-          TMIT
-          TMRF
-          TDRF
-          TTTOGF
+          QuoteDate          
           creator {
             _id
             email
@@ -82,6 +27,7 @@ const MyQuotations = () => {
     `,
   };
   useEffect(() => {
+    // Fetching data
     fetch("http://localhost:8000/graphql", {
       method: "POST",
       body: JSON.stringify(requestBody),
@@ -90,34 +36,39 @@ const MyQuotations = () => {
       },
     })
       .then((res) => {
+        // throwing an error if fetching is not successful
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Failed!");
         }
         return res.json();
       })
       .then((resData) => {
-        console.log(resData["data"]["events"]);
-        let k= resData["data"]["events"];
-        for (let i=0;i<k.length;i++)
+        // providing the quotes created by the user
+        let quotes = resData["data"]["events"];
+        for (let i = 0; i < quotes.length; i++)
         {
-            if(k[i]["creator"]["_id"]!==context.userId)
-              delete k[i];
+          // Deleting the quotes not created by the user
+          if(quotes[i]["creator"]["_id"] !== context.userId)
+              delete quotes[i];
         }
-        //console.log(k);
-        setData(k);
-        if(k.length==0)
+        // Updating data with the quotes created by the user
+        setData(quotes);
+        // Updating haveData variable
+        if(quotes.length === 0)
           setHaveData(false);
       })
       .catch((err) => {
         console.log(err);
+        // Updating isError variable
         setError(err);
       })
+        // updating isLoading variable
       .finally(() => {
         setIsLoading(false);
       });
   }, []);
   if (isLoading) return "Loading......";
-  if (isError) return "Error.....";
+  if (isError !== null) return "Error.....";
   return (
     <div>
       {isLoading && <h1>Quotations page is Loading</h1>}
@@ -126,8 +77,14 @@ const MyQuotations = () => {
         {data.map((d) => (
           <div className="blog-preview" key={d._id}>
             <Link to={`/middleware/${d._id}`}>
-              <h2>{d.address}</h2>
-              <p>{d.QuoteDate}</p>
+              <Grid style={{ flexBasis: "100%", padding: "8px"}}>
+                <Box style={{ lineHeight: "unset", color: "lightcoral", backgroundColor: "#5aa950"}} className="bold">{d.address}</Box>
+                <Box style={{ lineHeight: "unset" }}>Date: {d.QuoteDate}</Box>
+                <Box style={{ lineHeight: "unset" }}>Purchase Amount: {d.purchaseAmount}</Box>
+                <Box style={{ lineHeight: "unset" }}>Loan Amount: {d.loanAmount}</Box>
+              </Grid>
+              {/*<h2>{d.address}</h2>*/}
+              {/*<p>{d.QuoteDate}</p>*/}
             </Link>
           </div>
         ))}
